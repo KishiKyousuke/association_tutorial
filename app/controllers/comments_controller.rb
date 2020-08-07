570a046394121df1db17bb17e2bc853a673c6846
KishiKyousuke <carp011808pg@icloud.com>
2020-08-08 00:47:44 +0900

    コメント投稿機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    comment = current_user.comments.build(tweet_id: params[:tweet_id], body: comment_params[:body])
+    comment.save
+    redirect_to tweet_path(params[:tweet_id])
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    @user = User.find(@comment.user_id)
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to tweet_path(comment.tweet_id)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
