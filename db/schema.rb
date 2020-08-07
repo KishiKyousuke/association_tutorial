a311eb0002657fc145ec18434df1ad97935e46ac
KishiKyousuke <carp011808pg@icloud.com>
2020-08-07 10:38:19 +0900

    Comments中間テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_085942) do
+ActiveRecord::Schema.define(version: 2020_08_07_013448) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
